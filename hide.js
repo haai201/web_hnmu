16b18fc95ac079f3e888859222e49189ad2551ff
Nha <nhaduongvan5@gmail.com>
2021-10-01 13:14:59 +0700

    dvnha

---
+++ hide.js
@@ -0,0 +1,17 @@
+function HPTC() {
+    var x = document.getElementById("table");
+    if (x.style.display === "none") {
+        x.style.visibility = "show";
+    } else {
+        x.style.visibility = "hidden";
+    }
+}
+
+function HPTH() {
+    var y = document.getElementById("table1");
+    if (y.style.display === "none") {
+        y.style.visibility = "show";
+    } else {
+        y.style.visibility = "hidden";
+    }
+}
